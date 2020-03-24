9e130d484421a11b45e6884b2780194ffcb59173
Asif Mohammad Mollah <admin@mrasif.in>
2020-03-24 20:46:43 +0530

    chore: carrierwave rails implemented

---
+++ lib/carrierwave/support/uri_filename.rb
@@ -0,0 +1,10 @@
+module CarrierWave
+    module Support
+        module UriFilename
+            def self.filename(url)
+                path = url.split('?').first
+                URI.decode(path).gsub(%r{.*/(.*?$)}, '\1')
+            end
+        end
+    end
+end
