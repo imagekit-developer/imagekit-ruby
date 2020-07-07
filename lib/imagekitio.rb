ce5754b24330cd2beb47ac12bb11985f10a5b93b
Asif Mohammad Mollah <admin@mrasif.in>
2020-07-07 20:52:01 +0530

    fix: upload dir issue

--- lib/imagekitio.rb
+++ lib/imagekitio.rb
@@ -29,6 +29,16 @@ module CarrierWave
           if options!=nil
             @options=options
           end
+          folder=nil
+          begin
+            folder=store_dir
+          rescue
+          end
+
+          if folder!=nil
+            @options[:folder]=folder
+          end
+
           if self.file!=nil
             base64=Base64.encode64(::File.open(self.file.file, "rb").read)
             resp=@imagekit.upload_file(open(self.file.file,'rb'),self.file.filename,@options)
@@ -64,6 +74,10 @@ module CarrierWave
   
         def options
           options={}
+        end
+
+        def store_dir
+          store_dir=nil
         end
       end
   
