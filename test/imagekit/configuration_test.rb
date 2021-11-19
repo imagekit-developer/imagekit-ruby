dccb3eef831b622d094c8042b6e79377f54c3e8a
Bisho silwal <aayussilwal@gmail.com>
2021-11-19 16:04:39 +0545

    add configuration test

---
+++ test/imagekit/configuration_test.rb
@@ -0,0 +1,64 @@
+require_relative './spec_helper'
+require 'rspec/autorun'
+
+RSpec.describe ImageKitIo::Configurable do
+  let(:configurable_obj) { Class.new.send :include, ImageKitIo::Configurable}
+
+  describe 'Configuration' do
+    it 'configure with the keys' do
+      configurable_obj.configure do |config|
+        config.public_key = 'public_key_123'
+      end
+      expect(configurable_obj.config.public_key).to eq('public_key_123')
+      # test the default service
+      expect(configurable_obj.config.service).to eq(:carrierwave)
+    end
+
+    it 'configure without keys' do
+      expect(configurable_obj.config.service).to eq(:carrierwave)
+      expect(configurable_obj.config.private_key).to eq(nil)
+    end
+
+    context 'constants' do
+      let(:constants) { configurable_obj.constants}
+
+      it 'returns constant class' do
+        expect(constants).to eq(ImageKitIo::Constant)
+      end
+
+      it "returns default key's value" do
+        expect(constants.BASE_URL).to eq("https://api.imagekit.io/v1/files")
+      end
+
+      it "supports customization" do
+        constants.BASE_URL = 'imagekit.io'
+        expect(constants.BASE_URL).to eq('imagekit.io')
+        configurable_obj.configure do |config|
+          config.constants.BASE_URL = 'api.imagekit.io'
+        end
+        expect(configurable_obj.constants.BASE_URL).to eq('api.imagekit.io')
+      end
+    end
+
+    context 'services' do
+      it 'configures carrierwave by default' do
+        expect(configurable_obj.config.service).to eq(:carrierwave)
+        expect(configurable_obj).to receive(:require_relative).with('../carrierwave/carrierwave')
+        expect(configurable_obj).to_not receive(:require_relative).with('../active_storage/active_storage')
+        configurable_obj.configure do |config|
+          config.private_key = 'private_key_123'
+        end
+      end
+
+      it 'supports active_storage configuration' do
+        expect(configurable_obj).to_not receive(:require_relative).with('../carrierwave/carrierwave')
+        expect(configurable_obj).to receive(:require_relative).with('../active_storage/active_storage')
+        configurable_obj.configure do |config|
+          config.private_key = 'private_key_123'
+          config.service = :active_storage
+        end
+        expect(configurable_obj.config.service).to eq(:active_storage)
+      end
+    end
+  end
+end
--- test/imagekit/spec_helper.rb
+++ test/imagekit/spec_helper.rb
@@ -5,21 +5,10 @@ require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 require 'base64'
+require 'rspec'
 require 'webmock/rspec'
 require 'carrierwave'
 require 'byebug'
+require 'active_storage'
 
-require_relative '../../lib/imagekitio/constant'
-require_relative '../../lib/imagekitio/base'
-require_relative '../../lib/imagekitio/configurable'
-require_relative '../../lib/imagekitio/client'
-require_relative "../../lib/imagekitio/request"
-require_relative "../../lib/imagekitio/file"
-require_relative "../../lib/imagekitio/url"
-require_relative "../../lib/imagekitio/utils/calculation"
-
-ImageKitIo.configure do |config|
-  config.public_key = 'public_key_xyz'
-  config.private_key = 'private_key_xyz'
-  config.url_endpoint = 'https://ik.imagekit.io/xyz'
-end
+require_relative '../../lib/imagekitio'
